d1849434f105bbb455317ebb24d2397fc38e85f1
dell <IlhomjonIbragimov1502@gmail.com>
2021-12-10 10:23:59 +0500

    10.12.2021 test

---
+++ app/src/main/java/uz/ilhomjon2/damasuzdemo/LiniyaListYolovchiFragment.kt
@@ -0,0 +1,88 @@
+package uz.ilhomjon2.damasuzdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.*
+import uz.ilhomjon2.damasuzdemo.adapter.LiniyaAdapter
+import uz.ilhomjon2.damasuzdemo.databinding.FragmentLiniyaListYolovchiBinding
+import uz.ilhomjon2.damasuzdemo.models.Liniya
+import uz.ilhomjon2.damasuzdemo.models.SHopir
+import uz.ilhomjon2.damasuzdemo.models.Yolovchi
+
+class LiniyaListYolovchiFragment : Fragment() {
+    lateinit var binding: FragmentLiniyaListYolovchiBinding
+
+    lateinit var liniyaList: ArrayList<Liniya>
+    lateinit var yolovchi: Yolovchi
+
+    lateinit var firebaseDatabase: FirebaseDatabase
+    lateinit var referenceLiniya: DatabaseReference
+    lateinit var referenceShopir: DatabaseReference
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLiniyaListYolovchiBinding.inflate(layoutInflater)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        referenceLiniya = firebaseDatabase.getReference("liniya")
+        referenceShopir = firebaseDatabase.getReference("shopir")
+
+        binding.progressLiniya.visibility = View.VISIBLE
+        yolovchi = arguments?.getSerializable("keyYolovchi") as Yolovchi
+
+        referenceLiniya.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                liniyaList = ArrayList<Liniya>()
+
+                val children = snapshot.children
+                for (child in children) {
+                    val value = child.getValue(Liniya::class.java)
+                    if (value != null) {
+                        liniyaList.add(value)
+                    }
+                }
+                binding.progressLiniya.visibility = View.GONE
+                if (liniyaList.isEmpty()) {
+                    binding.tvEmpty.visibility = View.VISIBLE
+                }
+
+               referenceShopir.addValueEventListener(object : ValueEventListener{
+                   override fun onDataChange(snapshot: DataSnapshot) {
+                       val shList = ArrayList<SHopir>()
+                       val children1 = snapshot.children
+                       for (ch in children1) {
+                           val value = ch.getValue(SHopir::class.java)
+                           if (value!=null){
+                               shList.add(value)
+                           }
+                       }
+                       val liniyaAdapter = LiniyaAdapter(liniyaList, shList, object : LiniyaAdapter.OnCLick{
+                           override fun rootCLick(liniya: Liniya) {
+                                findNavController().navigate(R.id.mapYolovchiFragment, bundleOf("keyYolovchi" to yolovchi, "keyLiniya" to liniya))
+                           }
+                       })
+                       binding.rvLiniya.adapter = liniyaAdapter
+                   }
+
+                   override fun onCancelled(error: DatabaseError) {
+
+                   }
+               })
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Iltimos internetga qayta ulaning...", Toast.LENGTH_SHORT).show()
+            }
+        })
+        return binding.root
+    }
+}
---
+++ app/src/main/java/uz/ilhomjon2/damasuzdemo/models/Yolovchi.kt
@@ -0,0 +1,42 @@
+package uz.ilhomjon2.damasuzdemo.models
+
+import java.io.Serializable
+
+class Yolovchi : Serializable{
+    var id:String? = null
+    var name:String? = null
+    var number:String? = null
+    var location:MyLatLng? = null
+    var liniyaId:String? = null
+
+
+    constructor()
+    constructor(id: String?, name: String?, number: String?) {
+        this.id = id
+        this.name = name
+        this.number = number
+    }
+
+    constructor(id: String?, name: String?, number: String?, location: MyLatLng?) {
+        this.id = id
+        this.name = name
+        this.number = number
+        this.location = location
+    }
+}
+
+class MyLatLng: Serializable {
+    var latitude:Double? = null
+    var longitude:Double? = null
+
+    constructor(latitude: Double?, longitude: Double?) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+
+    constructor()
+
+    override fun toString(): String {
+        return "MyLatLng(latitude=$latitude, longitude=$longitude)"
+    }
+}
